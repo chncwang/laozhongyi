d187e11978ca356344a73a727585f6641db90f75
Chauncey Wang <chncwang@gmail.com>
2023-03-22 14:04:59 +0800

    Add hyperParametersToScore field to SimulatedAnnealingCheckPointData constructor and override toString and deepCopy methods

--- src/main/java/com/hljunlp/laozhongyi/checkpoint/SimulatedAnnealingCheckPointData.java
+++ src/main/java/com/hljunlp/laozhongyi/checkpoint/SimulatedAnnealingCheckPointData.java
@@ -10,8 +10,10 @@ public class SimulatedAnnealingCheckPointData extends CheckPointData {
                                             final Map<String, String> currentHyperParameters,
                                             final int currentHyperParametersIndex,
                                             final Map<String, String> bestHyperParameters,
-                                            final float bestScore) {
-        super(currentHyperParameters, currentHyperParametersIndex, bestHyperParameters, bestScore);
+                                            final float bestScore,
+                                            final Map<Map<String, String>, Float> hyperParametersToScore) {
+        super(currentHyperParameters, currentHyperParametersIndex, bestHyperParameters, bestScore
+                , hyperParametersToScore);
         this.temperature = temperature;
         this.decayRate = decayRate;
     }
@@ -22,5 +24,24 @@ public class SimulatedAnnealingCheckPointData extends CheckPointData {
 
     public float getDecayRate() {
         return decayRate;
+    }
+
+    @Override
+    public String toString() {
+        return "SimulatedAnnealingCheckPointData{" +
+                "temperature=" + temperature +
+                ", decayRate=" + decayRate +
+                "} " + super.toString();
+    }
+
+    @Override
+    public CheckPointData deepCopy() {
+        CheckPointData checkPointData = super.deepCopy();
+        return new SimulatedAnnealingCheckPointData(temperature, decayRate,
+                checkPointData.getCurrentHyperParameters(),
+                checkPointData.getCurrentHyperParametersIndex(),
+                checkPointData.getBestHyperParameters(),
+                checkPointData.getBestScore(),
+                checkPointData.getHyperParametersToScore());
     }
 }
